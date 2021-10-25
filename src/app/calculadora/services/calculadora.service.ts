b36c12f130525baf3aa67e11958d568aaed24094
Ivan Júnior <ivan.junior2706@gmail.com>
2021-10-25 20:25:24 -0300

    Implementando a função para calculos da calculadora no service.

--- src/app/calculadora/services/calculadora.service.ts
+++ src/app/calculadora/services/calculadora.service.ts
@@ -5,5 +5,33 @@ import { Injectable } from '@angular/core';
 })
 export class CalculadoraService {
 
+  static readonly SOMA: string = "+";
+  static readonly SUBSTRACAO: string = "-";
+  static readonly MULTIPLICACAO: string = "*";
+  static readonly DIVISAO: string = "/";
+
   constructor() { }
+
+  calcular(num1: number, num2: number, operacao: string): number {
+    let resultado: number;
+
+    switch (operacao) {
+      case CalculadoraService.SOMA:
+        resultado = num1 + num2;
+      break;
+      case CalculadoraService.SUBSTRACAO:
+        resultado = num1 - num2;
+      break
+      case CalculadoraService.DIVISAO:
+        resultado = num1 / num2;
+      break
+      case CalculadoraService.MULTIPLICACAO:
+        resultado = num1 * num2;
+        break;
+      default:
+        resultado = 0;
+    }
+
+    return resultado;
+  }
 }
