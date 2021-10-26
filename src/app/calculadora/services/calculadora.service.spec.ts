00684f1f24a52fb508f66100ce301d2e283ae98d
Ivan Júnior <ivan.junior2706@gmail.com>
2021-10-25 21:04:42 -0300

    Criando testes para o service.

--- src/app/calculadora/services/calculadora.service.spec.ts
+++ src/app/calculadora/services/calculadora.service.spec.ts
@@ -13,4 +13,30 @@ describe('CalculadoraService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should be 4 + 5 = 9', () =>{
+    let soma = service.calcular(4, 5, CalculadoraService.SOMA);
+    expect(soma).toEqual(9);
+  });
+
+  it('should be 4 - 5 = -1', () =>{
+    let substracao = service.calcular(4, 5, CalculadoraService.SUBSTRACAO);
+    expect(substracao).toEqual(-1);
+  });
+
+  it('should be 1 * -1 = -1', () =>{
+    let multiplicacao = service.calcular(1, -1, CalculadoraService.MULTIPLICACAO);
+    expect(multiplicacao).toEqual(-1);
+  });
+
+  it('should be 1 / 4 = 0.25', () =>{
+    let divisao = service.calcular(1, 4, CalculadoraService.DIVISAO);
+    expect(divisao).toEqual(0.25);
+  });
+
+  it('should be invalide operation', () =>{
+    let operacao = '%'
+    let divisao = service.calcular(1, 4, operacao);
+    expect(divisao).toEqual(0);
+  });
 });
